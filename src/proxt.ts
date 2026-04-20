e63a5975b82e0574d38bb84de756f931b7931940
vishalprajapati15 <vishalprajapat3266@gmail.com>
2026-04-20 14:26:19 +0530

    Next-Auth added

---
+++ src/proxt.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server'
+export { default } from "next-auth/middleware"
+import { getToken } from 'next-auth/jwt'
+
+export async function proxy(request: NextRequest) {
+
+    const token = await getToken({ req: request });
+    const url = request.nextUrl;
+    if (token &&
+        (
+            url.pathname.startsWith('/sign-in') ||
+            url.pathname.startsWith('/sign-up') ||
+            url.pathname.startsWith('/verify') ||
+            url.pathname.startsWith('/')
+        )
+    ) {
+        return NextResponse.redirect(new URL('/dashboard', request.url))
+    }
+
+    if (!token && url.pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/sign-in', request.url))
+    }
+
+    return NextResponse.next();
+}
+
+// Alternatively, you can use a default export:
+// export default function proxy(request: NextRequest) { ... }
+
+export const config = {
+    matcher: ['/sign-in', '/sign-up', '/', '/dashboard/:path*', '/verify/:path*']
+}
