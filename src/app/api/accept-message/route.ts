871b63fc1f9b4aa681d12c8056dc934cb3694865
vishalprajapati15 <vishalprajapat3266@gmail.com>
2026-04-20 17:04:45 +0530

    add accept message status update and retrieval API

---
+++ src/app/api/accept-message/route.ts
@@ -0,0 +1,90 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/user.model";
+import { User } from "next-auth";
+
+
+export async function POST(request: Request) {
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User
+
+    if (!session || !session.user) {
+        return Response.json({
+            success: false,
+            message: 'Not Authenticated!!'
+        }, { status: 401 });
+    }
+
+    const userId = user._id;
+
+    const { acceptMessage } = await request.json();
+
+    try {
+        const updatedUser = await UserModel.findByIdAndUpdate(
+            userId,
+            { isAcceptingMessage: acceptMessage },
+            { new: true }
+        )
+
+        if (!updatedUser) {
+            return Response.json({
+                success: false,
+                message: 'Failed to update user status to accept message!!'
+            }, { status: 401 });
+        }
+
+        return Response.json({
+            success: true,
+            message: 'message acceptance status updated successfully!!',
+            updatedUser
+        }, { status: 200 });
+
+    } catch (error) {
+        console.log("Failed to update user status to accept message!! : ", error);
+        return Response.json({
+            success: false,
+            message: 'Failed to update user status to accept message!!!!'
+        }, { status: 500 });
+    }
+}
+
+
+
+export async function GET(request: Request) {
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User
+
+    if (!session || !session.user) {
+        return Response.json({
+            success: false,
+            message: 'Not Authenticated!!'
+        }, { status: 401 });
+    }
+
+    const userId = user._id;
+
+    try {
+        const foundUser = await UserModel.findById(userId);
+
+        if (!foundUser) {
+            return Response.json({
+                success: false,
+                message: 'User not found!!'
+            }, { status: 404 });
+        }
+        return Response.json({
+            success: true,
+            message: 'User Fetched successfully!!',
+            isAcceptingMessage: foundUser.isAcceptingMessage
+        }, { status: 200 });
+    } catch (error) {
+        console.log("Failed to fetch user status to accept message!! : ", error);
+        return Response.json({
+            success: false,
+            message: 'Failed to fetch user status to accept message!!!!'
+        }, { status: 500 });
+    }
+}
--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -1,4 +1,56 @@
 import dbConnect from "@/lib/dbConnect";
-import { z } from 'zod'
 import UserModel from "@/model/user.model";
-import { usernameValidation } from "@/schemas/signUpSchema";
+
+
+
+export async function POST(request: Request) {
+    await dbConnect()
+    try {
+        const { username, code } = await request.json();
+
+        const decodedUserName = decodeURIComponent(username);
+
+        const user = await UserModel.findOne({ username: decodedUserName })
+
+        if (!user) {
+            return Response.json({
+                success: false,
+                message: 'Username not found'
+            }, { status: 400 });
+        }
+
+        const isCodeValid = user.verifyCode === code;
+        const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+        if (isCodeValid && isCodeNotExpired) {
+            user.isVerified = true;
+            await user.save();
+
+            return Response.json({
+                success: true,
+                message: 'User verified successfully!!'
+            }, { status: 200 });
+        }
+        else if (!isCodeNotExpired) {
+            return Response.json({
+                success: false,
+                message: 'Verification code is expired. Please sign-up again!!'
+            }, { status: 400 });
+        }
+        else {
+            return Response.json({
+                success: false,
+                message: 'Incorrect verification Code!!'
+            }, { status: 400 });
+        }
+
+
+
+    } catch (error) {
+        console.error("error verifying username : ", error);
+        return Response.json({
+            success: false,
+            message: 'Error verifying username!!'
+        }, { status: 500 });
+    }
+}
