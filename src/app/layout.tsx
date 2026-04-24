71d5988c46272778a5721a072686529952d7d83f
vishalprajapati15 <vishalprajapat3266@gmail.com>
2026-04-24 10:52:45 +0530

    Add Message page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
 import {Toaster} from 'sonner';
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
     >
       <AuthProvider>
         <body className="min-h-full flex flex-col">
+          {/* <Navbar /> */}
           {children}
           <Toaster/>
         </body>
